9a441dc33ecb5e8f9a221144d85ecefcc6f79198
Joseph Oldfield <josepholdfield2597@gmail.com>
2020-02-23 14:39:36 +0000

    Displaying Quiz Questions and Handling Events

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {this.props.quiz_questions.map(button_text => <QuizQuestionButton {...button_text.answer_options}/>)}/>
                     </ul>
                 </section>
             </main>
