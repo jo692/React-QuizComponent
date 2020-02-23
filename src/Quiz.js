9fe92523ac26179e42cc1617dd8719237209f1cd
Joseph Oldfield <josepholdfield2597@gmail.com>
2020-02-23 14:02:11 +0000

    Creating a QuizQuestion Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,16 +4,16 @@ import QuizQuestion from "./QuizQuestion.js"
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    render() {
-        return (
-            <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[0].instruction_text}/>
-            </div>
-        );
-    }
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1};
+    }
+    render() {
+        return (
+            <div>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            </div>
+        );
     }
 }
 
